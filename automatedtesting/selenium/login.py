f2511f788f3d997552adc55a2e9f5873b371dc29
dawitanelay <57917956+dawitanelay@users.noreply.github.com>
2022-11-12 00:55:39 +0100

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,80 +1,69 @@
 # #!/usr/bin/env python
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 
+def get_current_end_point(url: str) -> str:
+    return url.split("/")[-1]
 
-now = datetime.now()
-current_date_time = now.strftime("%d/%m/%Y %H:%M:%S")
-
-# Start the browser and login with standard_user
-options = ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(options=options)
-# driver = webdriver.Chrome()
-
-
-def login(user, password):
+def login (driver, user, password):
     """
-    Login to website
+    Start the login with standard_user.
     """
-    print('Starting the browser...')
-    print("Current Timestamp ", current_date_time)
+    print(f"[*] Login in as {user}")
+    driver.find_element(By.ID, 'user-name').send_keys(user)
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.ID, 'login-button').click()
+    assert get_current_end_point(driver.current_url) == "inventory.html"
 
-    # --uncomment when running in Azure DevOps.
-    print('Browser started successfully. Navigating to the demo page to login.')
-    driver.get('https://www.saucedemo.com/')
-
-    driver.find_element(
-        By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
-    driver.find_element(
-        By.CSS_SELECTOR, "input[id='password']").send_keys(password)
-    driver.find_element(
-        By.CSS_SELECTOR, "input[id='login-button']").click()
-    home_url = "https://www.saucedemo.com/inventory.html"
-    if home_url == driver.current_url:
-        print(f"{current_date_time} ---> Login succesfull!!")
-    else:
-        print(f"{current_date_time} ---> Login Unsuccesfull!!")
-
-
-def add_to_cart(items):
+def add_items(driver):
     """
-    add product to cart
+    Add 6 items to the cart.
     """
-    print(f'Adding {len(items)} in the cart')
-    for item in range(len(items)):
-        driver.find_element(
-            By.CSS_SELECTOR, "a[id='item_" + str(item) + "_title_link']").click()
-        driver.find_element(
-            By.CSS_SELECTOR, "button[class='btn btn_primary btn_small btn_inventory']").click()
-        name = driver.find_element(
-            By.CLASS_NAME, "inventory_details_name").text
-        print(f'{current_date_time} ----> {name} ----> Added to cart')
-        driver.find_element(
-            By.CSS_SELECTOR, "button[id='back-to-products']").click()
+    assert get_current_end_point(driver.current_url) == "inventory.html"
+    print(f"[*] Adding items to cart.")
+    inventory_list = driver.find_elements(By.CLASS_NAME, "inventory_item")
+    for item in inventory_list:
+        item_text = item.find_element(By.CLASS_NAME, 'inventory_item_name').text
+        print(f"\t- {item_text} was added.")
+        item.find_element(By.CLASS_NAME, 'btn_inventory').click()
+    items_count = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+    assert int(items_count) == 6
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+    assert get_current_end_point(driver.current_url) == "cart.html"
 
-
-def remove_products(items):
+def remove_items(driver):
     """
-    Removing all the products from cart
+    Remove added items from cart.
     """
-    for item in range(len(items)):
-        driver.find_element(
-            By.CSS_SELECTOR, "a[id='item_" + str(item) + "_title_link']").click()
-        driver.find_element(
-            By.CSS_SELECTOR, "button[class='btn btn_secondary btn_small btn_inventory']").click()
-        name = driver.find_element(
-            By.CLASS_NAME, "inventory_details_name").text
-        print(f'{current_date_time} ----> {name} ----> Removed from cart')
-        driver.find_element(
-            By.CSS_SELECTOR, "button[id='back-to-products']").click()
+    assert get_current_end_point(driver.current_url) == "cart.html"
+    print(f"[*] Removing items to cart.")
+    cart_list = driver.find_elements(By.CLASS_NAME, "cart_item")
+    for item in cart_list:
+        item_text = item.find_element(By.CLASS_NAME, 'inventory_item_name').text
+        print(f"\t- {item_text} was removed.")
+        item.find_element(By.CLASS_NAME, 'cart_button').click()
+    items_count = len(driver.find_elements(By.CLASS_NAME, "cart_item"))
+    assert int(items_count) == 0
+    print(f"[✓] All items were removed successfully.")
 
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
 
 if __name__ == "__main__":
-    login('standard_user', 'secret_sauce')
-    items = driver.find_elements(By.CLASS_NAME, "inventory_item_price")
-    add_to_cart(items)
-    remove_products(items)
+    print ('Starting the browser...')
+    options = ChromeOptions()
+    options.add_argument("--no-sandbox") 
+    options.add_argument("--disable-dev-shm-usage") 
+    options.add_argument("--headless") 
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=options)
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+    login(driver, 'standard_user', 'secret_sauce')
+    add_items(driver)
+    remove_items(driver)
+    print("[✓] All tests ran successfully.")
